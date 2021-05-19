4fac9f37d8714391436e12b7d6effc7ee238da03
Leon Lynch <lynch.leon@gmail.com>
2021-05-19 18:40:48 +0200

    Add EMV tag formats for EMV 4.3 Book 1, Annex B

--- src/emv_tlv.c
+++ src/emv_tlv.c
@@ -40,18 +40,21 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 				"The number that identifies the major industry and the card "
 				"issuer and that forms the first part of the Primary Account "
 				"Number (PAN)";
+			info->format = EMV_FORMAT_N;
 			return 0;
 
 		case EMV_TAG_4F_APPLICATION_DF_NAME:
 			info->tag_name = "Application Dedicated File (ADF) Name";
 			info->tag_desc =
 				"Identifies the application as described in ISO/IEC 7816-4";
+			info->format = EMV_FORMAT_B;
 			return 0;
 
 		case EMV_TAG_50_APPLICATION_LABEL:
 			info->tag_name = "Application Label";
 			info->tag_desc =
 				"Mnemonic associated with the AID according to ISO/IEC 7816-4";
+			info->format = EMV_FORMAT_ANS;
 			return 0;
 
 		case EMV_TAG_61_APPLICATION_TEMPLATE:
@@ -59,17 +62,20 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 			info->tag_desc =
 				"Contains one or more data objects relevant to an application "
 				"directory entry according to ISO/IEC 7816-4";
+			info->format = EMV_FORMAT_B;
 			return 0;
 
 		case EMV_TAG_6F_FCI_TEMPLATE:
 			info->tag_name = "File Control Information (FCI) Template";
 			info->tag_desc =
 				"Identifies the FCI template according to ISO/IEC 7816-4";
+			info->format = EMV_FORMAT_VAR;
 			return 0;
 
 		case EMV_TAG_70_DATA_TEMPLATE:
 			info->tag_name = "EMV Data Template";
 			info->tag_desc = "Contains EMV data";
+			info->format = EMV_FORMAT_VAR;
 			return 0;
 
 		case EMV_TAG_73_DIRECTORY_DISCRETIONARY_TEMPLATE:
@@ -77,6 +83,7 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 			info->tag_desc =
 				"Issuer discretionary part of the directory according to "
 				"ISO/IEC 7816-4";
+			info->format = EMV_FORMAT_VAR;
 			return 0;
 
 		case EMV_TAG_84_DF_NAME:
@@ -84,6 +91,7 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 			info->tag_desc =
 				"Identifies the name of the Dedicated File (DF) as described "
 				"in ISO/IEC 7816-4";
+			info->format = EMV_FORMAT_B;
 			return 0;
 
 		case EMV_TAG_87_APPLICATION_PRIORITY_INDICATOR:
@@ -91,6 +99,7 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 			info->tag_desc =
 				"Indicates the priority of a given application or group of "
 				"applications in a directory";
+			info->format = EMV_FORMAT_B;
 			return 0;
 
 		case EMV_TAG_88_SFI:
@@ -101,6 +110,7 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 				"or Directory Definition File (DDF). It is a binary data "
 				"object having a value in the range 1 - 30 and with the three "
 				"high order bits set to zero.";
+			info->format = EMV_FORMAT_B;
 			return 0;
 
 		case EMV_TAG_9D_DDF_NAME:
@@ -108,6 +118,7 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 			info->tag_desc =
 				"Identifies the name of a Dedicated File (DF) associated with "
 				"a directory";
+			info->format = EMV_FORMAT_B;
 			return 0;
 
 		case EMV_TAG_A5_FCI_PROPRIETARY_TEMPLATE:
@@ -116,6 +127,7 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 				"Identifies the data object proprietary to this specification "
 				"in the File Control Information (FCI) template according to "
 				"ISO/IEC 7816-4";
+			info->format = EMV_FORMAT_VAR;
 			return 0;
 
 		case EMV_TAG_5F2D_LANGUAGE_PREFERENCE:
@@ -124,6 +136,7 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 				"1-4 languages stored in order of preference, each "
 				"represented by 2 alphabetical characters according to "
 				"ISO 639";
+			info->format = EMV_FORMAT_AN;
 			return 0;
 
 		case EMV_TAG_5F50_ISSUER_URL:
@@ -131,6 +144,7 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 			info->tag_desc =
 				"The URL provides the location of the issuer's Library Server "
 				"on the Internet";
+			info->format = EMV_FORMAT_ANS;
 			return 0;
 
 		case EMV_TAG_5F53_IBAN:
@@ -138,12 +152,14 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 			info->tag_desc =
 				"Uniquely identifies the account of a customer at a financial "
 				"institution as defined in ISO 13616.";
+			info->format = EMV_FORMAT_VAR;
 			return 0;
 
 		case EMV_TAG_5F54_BANK_IDENTIFIER_CODE:
 			info->tag_name = "Bank Identifier Code (BIC)";
 			info->tag_desc =
 				"Uniquely identifies a bank as defined in ISO 9362.";
+			info->format = EMV_FORMAT_VAR;
 			return 0;
 
 		case EMV_TAG_5F55_ISSUER_COUNTRY_CODE_ALPHA2:
@@ -151,6 +167,7 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 			info->tag_desc =
 				"Indicates the country of the issuer as defined in ISO 3166 "
 				"(using a 2 character alphabetic code)";
+			info->format = EMV_FORMAT_A;
 			return 0;
 
 		case EMV_TAG_5F56_ISSUER_COUNTRY_CODE_ALPHA3:
@@ -158,12 +175,14 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 			info->tag_desc =
 				"Indicates the country of the issuer as defined in ISO 3166 "
 				"(using a 3 character alphabetic code)";
+			info->format = EMV_FORMAT_A;
 			return 0;
 
 		case EMV_TAG_9F06_AID:
 			info->tag_name = "Application Identifier (AID) - terminal";
 			info->tag_desc =
 				"Identifies the application as described in ISO/IEC 7816-4";
+			info->format = EMV_FORMAT_B;
 			return 0;
 
 		case EMV_TAG_9F11_ISSUER_CODE_TABLE_INDEX:
@@ -171,12 +190,14 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 			info->tag_desc =
 				"Indicates the code table according to ISO/IEC 8859 for "
 				"displaying the Application Preferred Name";
+			info->format = EMV_FORMAT_N;
 			return 0;
 
 		case EMV_TAG_9F12_APPLICATION_PREFERRED_NAME:
 			info->tag_name = "Application Preferred Name";
 			info->tag_desc =
 				"Preferred mnemonic associated with the AID";
+			info->format = EMV_FORMAT_ANS;
 			return 0;
 
 		case EMV_TAG_9F38_PDOL:
@@ -185,6 +206,7 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 				"Contains a list of terminal resident data objects (tags and "
 				"lengths) needed by the ICC in processing the GET PROCESSING "
 				"OPTIONS command";
+			info->format = EMV_FORMAT_B;
 			return 0;
 
 		case EMV_TAG_9F4D_LOG_ENTRY:
@@ -192,15 +214,18 @@ int emv_tlv_get_info(const emv_tlv_t* tlv, struct emv_tlv_info_t* info)
 			info->tag_desc =
 				"Provides the SFI of the Transaction Log file and its number "
 				"of records";
+			info->format = EMV_FORMAT_B;
 			return 0;
 
 		case EMV_TAG_BF0C_FCI_ISSUER_DISCRETIONARY_DATA:
 			info->tag_name = "File Control Information (FCI) Issuer Discretionary Data";
 			info->tag_desc =
 				"Issuer discretionary part of the File Control Information (FCI)";
+			info->format = EMV_FORMAT_VAR;
 			return 0;
 
 		default:
+			info->format = EMV_FORMAT_B;
 			return 1;
 	}
 }
--- src/emv_tlv.h
+++ src/emv_tlv.h
@@ -34,6 +34,70 @@ __BEGIN_DECLS
 typedef struct iso8825_tlv_t emv_tlv_t;
 
 /**
+ * EMV data element formats
+ * @remark See EMV 4.3 Book 1, 4.3
+ */
+enum emv_format_t {
+	/**
+	 * Alphabetic data.
+	 * Single character per byte.
+	 * a-z, A-Z only.
+	 */
+	EMV_FORMAT_A,
+
+	/**
+	 * Alphanumeric data.
+	 * Single character per byte.
+	 * a-z, A-Z, 0-9 only.
+	 */
+	EMV_FORMAT_AN,
+
+	/**
+	 * Alphanumeric special data.
+	 * Single character per byte.
+	 * Encoded using ISO/IEC 8859 common character set.
+	 *
+	 * @note The Application Label
+	 *       (@ref EMV_TAG_50_APPLICATION_LABEL) field is limited to
+	 *       a-z, A-Z, 0-9 and the space special character.
+	 *
+	 * @note The Application Preferred Name
+	 *       (@ref EMV_TAG_9F12_APPLICATION_PREFERRED_NAME) field
+	 *       may encode non-control characters from the ISO/IEC 8859 part
+	 *       designated by the Issuer Code Table Index field
+	 *       (@ref EMV_TAG_9F11_ISSUER_CODE_TABLE_INDEX).
+	 */
+	EMV_FORMAT_ANS,
+
+	/**
+	 * Fixed length binary data.
+	 * Encoded according to the field type.
+	 */
+	EMV_FORMAT_B,
+
+	/**
+	 * Compressed numeric data.
+	 * Two decimal digits per byte.
+	 * Left justified and padded with trailing 'F's.
+	 */
+	EMV_FORMAT_CN,
+
+	/**
+	 * Numeric data.
+	 * Two decimal digits per byte.
+	 * Right justified and padded with leading zeroes.
+	 * Also referred to as Binary Coded Decimal (BCD) or Unsigned Packed
+	 */
+	EMV_FORMAT_N,
+
+	/**
+	 * Variable length binary data.
+	 * Encoded according to the field type.
+	 */
+	EMV_FORMAT_VAR,
+};
+
+/**
  * EMV TLV information as human readable strings
  * @remark See EMV 4.3 Book 1, Annex B
  * @remark See EMV 4.3 Book 3, Annex A
@@ -42,6 +106,7 @@ typedef struct iso8825_tlv_t emv_tlv_t;
 struct emv_tlv_info_t {
 	const char* tag_name;                       ///< Tag name, if available. Otherwise NULL.
 	const char* tag_desc;                       ///< Tag description, if available. Otherwise NULL.
+	enum emv_format_t format;                   ///< Value format. @see emv_format_t
 };
 
 /**
