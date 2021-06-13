8eef12134643c1bd78a4543eca4ca00f512887b9
Leon Lynch <lynch.leon@gmail.com>
2021-06-13 10:24:50 +0200

    Add initial transaction object and terminal configuration for testing
    This is only intended as a convenient object to hold the terminal
    configuration required to transact, and to hold the data provided by
    card interactions. In future, this will be refactored to be defined by,
    and managed by, the EMV library.

--- tools/emv-tool.c
+++ tools/emv-tool.c
@@ -33,10 +33,24 @@
 #include "emv_ttl.h"
 #include "emv_app.h"
 #include "iso7816_strings.h"
-#include <string.h> // for memcpy() that should be removed later
+#include <string.h> // for memset() and memcpy() that should be removed later
+
+struct emv_txn_t {
+	// Terminal Transport Layer
+	struct emv_ttl_t ttl;
+
+	// Terminal configuration
+	struct emv_tlv_list_t terminal;
+
+	// Supported applications
+	struct emv_tlv_list_t supported_aids;
+};
+static struct emv_txn_t emv_txn;
 
 // Helper functions
 static const char* pcsc_get_reader_state_string(unsigned int reader_state);
+static void emv_txn_load_config(struct emv_txn_t* emv_txn);
+static void emv_txn_destroy(struct emv_txn_t* emv_txn);
 
 static const char* pcsc_get_reader_state_string(unsigned int reader_state)
 {
@@ -60,6 +74,43 @@ static const char* pcsc_get_reader_state_string(unsigned int reader_state)
 	return NULL;
 }
 
+static void emv_txn_load_config(struct emv_txn_t* emv_txn)
+{
+	// Terminal config
+	emv_tlv_list_push(&emv_txn->terminal, EMV_TAG_9F1A_TERMINAL_COUNTRY_CODE, 2, (uint8_t[]){ 0x05, 0x28 }, 0); // Netherlands
+	emv_tlv_list_push(&emv_txn->terminal, EMV_TAG_9F1B_TERMINAL_FLOOR_LIMIT, 4, (uint8_t[]){ 0x00, 0x00, 0x03, 0xE8 }, 0); // 1000
+	emv_tlv_list_push(&emv_txn->terminal, EMV_TAG_9F1C_TERMINAL_IDENTIFICATION, 8, (const uint8_t*)"emv-tool", 0); // Unique location of terminal at merchant
+	emv_tlv_list_push(&emv_txn->terminal, EMV_TAG_9F1E_IFD_SERIAL_NUMBER, 8, (const uint8_t*)"12345678", 0); // Serial number
+
+	// Terminal Capabilities:
+	// - Card Data Input Capability: IC with Contacts
+	// - CVM Capability: Plaintext offline PIN, Enciphered online PIN, Signature, Enciphered offline PIN, No CVM
+	// - Security Capability: SDA, DDA, CDA
+	emv_tlv_list_push(&emv_txn->terminal, EMV_TAG_9F33_TERMINAL_CAPABILITIES, 3, (uint8_t[]){ 0x20, 0xF8, 0xC8}, 0);
+
+	// Merchant attended, offline with online capability
+	emv_tlv_list_push(&emv_txn->terminal, EMV_TAG_9F35_TERMINAL_TYPE, 1, (uint8_t[]){ 0x22 }, 0);
+
+	// Additional Terminal Capabilities:
+	// - Transaction Type Capability: Goods, Services, Cashback, Cash, Inquiry, Payment
+	// - Terminal Data Input Capability: Numeric, Alphabetic and special character keys, Command keys, Function keys
+	// - Terminal Data Output Capability: Attended print, Attended display, Code table 1 to 10
+	emv_tlv_list_push(&emv_txn->terminal, EMV_TAG_9F40_ADDITIONAL_TERMINAL_CAPABILITIES, 5, (uint8_t[]){ 0xFA, 0x00, 0xF0, 0xA3, 0xFF }, 0);
+
+	// Supported applications
+	emv_tlv_list_push(&emv_txn->supported_aids, EMV_TAG_9F06_AID, 6, (uint8_t[]){ 0xA0, 0x00, 0x00, 0x00, 0x03, 0x10 }, EMV_ASI_PARTIAL_MATCH); // Visa
+	emv_tlv_list_push(&emv_txn->supported_aids, EMV_TAG_9F06_AID, 7, (uint8_t[]){ 0xA0, 0x00, 0x00, 0x00, 0x03, 0x20, 0x10 }, EMV_ASI_EXACT_MATCH); // Visa Electron
+	emv_tlv_list_push(&emv_txn->supported_aids, EMV_TAG_9F06_AID, 7, (uint8_t[]){ 0xA0, 0x00, 0x00, 0x00, 0x03, 0x20, 0x20 }, EMV_ASI_EXACT_MATCH); // V Pay
+	emv_tlv_list_push(&emv_txn->supported_aids, EMV_TAG_9F06_AID, 6, (uint8_t[]){ 0xA0, 0x00, 0x00, 0x00, 0x04, 0x10 }, EMV_ASI_PARTIAL_MATCH); // Mastercard
+	emv_tlv_list_push(&emv_txn->supported_aids, EMV_TAG_9F06_AID, 6, (uint8_t[]){ 0xA0, 0x00, 0x00, 0x00, 0x04, 0x30 }, EMV_ASI_PARTIAL_MATCH); // Maestro
+}
+
+static void emv_txn_destroy(struct emv_txn_t* emv_txn)
+{
+	emv_tlv_list_clear(&emv_txn->supported_aids);
+	emv_tlv_list_clear(&emv_txn->terminal);
+}
+
 int main(void)
 {
 	int r;
@@ -76,13 +127,13 @@ int main(void)
 	r = pcsc_init(&pcsc);
 	if (r) {
 		printf("PC/SC initialisation failed\n");
-		goto exit;
+		goto pcsc_exit;
 	}
 
 	pcsc_count = pcsc_get_reader_count(pcsc);
 	if (!pcsc_count) {
 		printf("No PC/SC readers detected\n");
-		goto exit;
+		goto pcsc_exit;
 	}
 
 	printf("PC/SC readers:\n");
@@ -109,11 +160,11 @@ int main(void)
 	r = pcsc_wait_for_card(pcsc, 5000, &reader_idx);
 	if (r < 0) {
 		printf("PC/SC error\n");
-		goto exit;
+		goto pcsc_exit;
 	}
 	if (r > 0) {
 		printf("No card; exiting\n");
-		goto exit;
+		goto pcsc_exit;
 	}
 
 	reader = pcsc_get_reader(pcsc, reader_idx);
@@ -122,45 +173,42 @@ int main(void)
 	r = pcsc_reader_connect(reader);
 	if (r) {
 		printf("PC/SC reader activation failed\n");
-		goto exit;
+		goto pcsc_exit;
 	}
 	printf("Card activated\n");
 
 	r = pcsc_reader_get_atr(reader, atr, &atr_len);
 	if (r) {
 		printf("Failed to retrieve ATR\n");
-		goto exit;
+		goto pcsc_exit;
 	}
 
 	r = iso7816_atr_parse(atr, atr_len, &atr_info);
 	if (r) {
 		printf("Failed to parse ATR\n");
-		goto exit;
+		goto pcsc_exit;
 	}
 
 	print_atr(&atr_info);
 
-	struct emv_ttl_t emv_ttl;
-	emv_ttl.cardreader.mode = EMV_CARDREADER_MODE_APDU;
-	emv_ttl.cardreader.ctx = reader;
-	emv_ttl.cardreader.trx = &pcsc_reader_trx;
+	// Prepare for EMV transaction
+	memset(&emv_txn, 0, sizeof(emv_txn));
+	emv_txn.ttl.cardreader.mode = EMV_CARDREADER_MODE_APDU;
+	emv_txn.ttl.cardreader.ctx = reader;
+	emv_txn.ttl.cardreader.trx = &pcsc_reader_trx;
+	emv_txn_load_config(&emv_txn);
 
-	// Supported applications
-	struct emv_tlv_list_t supported_aids = EMV_TLV_LIST_INIT;
-	emv_tlv_list_push(&supported_aids, EMV_TAG_9F06_AID, 6, (uint8_t[]){ 0xA0, 0x00, 0x00, 0x00, 0x03, 0x10 }, EMV_ASI_PARTIAL_MATCH); // Visa
-	emv_tlv_list_push(&supported_aids, EMV_TAG_9F06_AID, 7, (uint8_t[]){ 0xA0, 0x00, 0x00, 0x00, 0x03, 0x20, 0x10 }, EMV_ASI_EXACT_MATCH); // Visa Electron
-	emv_tlv_list_push(&supported_aids, EMV_TAG_9F06_AID, 7, (uint8_t[]){ 0xA0, 0x00, 0x00, 0x00, 0x03, 0x20, 0x20 }, EMV_ASI_EXACT_MATCH); // V Pay
-	emv_tlv_list_push(&supported_aids, EMV_TAG_9F06_AID, 6, (uint8_t[]){ 0xA0, 0x00, 0x00, 0x00, 0x04, 0x10 }, EMV_ASI_PARTIAL_MATCH); // Mastercard
-	emv_tlv_list_push(&supported_aids, EMV_TAG_9F06_AID, 6, (uint8_t[]){ 0xA0, 0x00, 0x00, 0x00, 0x04, 0x30 }, EMV_ASI_PARTIAL_MATCH); // Maestro
+	printf("\nTerminal config:\n");
+	print_emv_tlv_list(&emv_txn.terminal);
 
 	// Candidate applications for selection
 	struct emv_app_list_t app_list = EMV_APP_LIST_INIT;
 
 	printf("\nSELECT Payment System Environment (PSE)\n");
-	r = emv_tal_read_pse(&emv_ttl, &app_list);
+	r = emv_tal_read_pse(&emv_txn.ttl, &app_list);
 	if (r < 0) {
 		printf("Failed to read PSE; terminate session\n");
-		goto exit;
+		goto emv_exit;
 	}
 	if (r > 0) {
 		printf("Failed to read PSE; continue session\n");
@@ -173,27 +221,25 @@ int main(void)
 	}
 
 	// Filter for supported applications
-	emv_app_list_filter_supported(&app_list, &supported_aids);
+	emv_app_list_filter_supported(&app_list, &emv_txn.supported_aids);
 
 	// If PSE failed or no apps found by PSE
 	// See EMV 4.3 Book 1, 12.3.2, step 5
 	if (r > 0 || emv_app_list_is_empty(&app_list)) {
 		printf("\nFind supported AIDs\n");
-		r = emv_tal_find_supported_apps(&emv_ttl, &supported_aids, &app_list);
+		r = emv_tal_find_supported_apps(&emv_txn.ttl, &emv_txn.supported_aids, &app_list);
 		if (r) {
 			printf("Failed to find supported AIDs; terminate session\n");
-			emv_tlv_list_clear(&supported_aids);
-			goto exit;
+			goto emv_exit;
 		}
 	}
-	emv_tlv_list_clear(&supported_aids);
 
 	if (emv_app_list_is_empty(&app_list)) {
 		printf("No supported applications\n");
-		goto exit;
+		goto emv_exit;
 	}
 
-	printf("Supported applications:\n");
+	printf("Candidate applications:\n");
 	for (struct emv_app_t* app = app_list.front; app != NULL; app = app->next) {
 		print_emv_app(app);
 	}
@@ -206,7 +252,7 @@ int main(void)
 		struct emv_app_t* current_app = emv_app_list_pop(&app_list);
 		if (!current_app) {
 			printf("No supported applications\n");
-			goto exit;
+			goto emv_exit;
 		}
 		emv_app_list_clear(&app_list);
 
@@ -221,17 +267,17 @@ int main(void)
 		uint8_t fci[EMV_RAPDU_DATA_MAX];
 		size_t fci_len = sizeof(fci);
 		uint16_t sw1sw2;
-		r = emv_ttl_select_by_df_name(&emv_ttl, current_aid, current_aid_len, fci, &fci_len, &sw1sw2);
+		r = emv_ttl_select_by_df_name(&emv_txn.ttl, current_aid, current_aid_len, fci, &fci_len, &sw1sw2);
 		if (r) {
 			printf("Failed to select application; r=%d\n", r);
-			goto exit;
+			goto emv_exit;
 		}
 		print_buf("FCI", fci, fci_len);
 		print_emv_buf(fci, fci_len, "  ", 0);
 		printf("SW1SW2 = %04hX (%s)\n", sw1sw2, iso7816_sw1sw2_get_string(sw1sw2 >> 8, sw1sw2 & 0xff, str, sizeof(str)));
 
 		if (sw1sw2 != 0x9000) {
-			goto exit;
+			goto emv_exit;
 		}
 
 		// Create EMV application object
@@ -239,7 +285,7 @@ int main(void)
 		app = emv_app_create_from_fci(fci, fci_len);
 		if (r) {
 			printf("emv_app_populate_from_fci() failed; r=%d\n", r);
-			goto exit;
+			goto emv_exit;
 		}
 		print_emv_app(app);
 		print_emv_tlv_list(&app->tlv_list);
@@ -249,10 +295,12 @@ int main(void)
 	r = pcsc_reader_disconnect(reader);
 	if (r) {
 		printf("PC/SC reader deactivation failed\n");
-		goto exit;
+		goto emv_exit;
 	}
 	printf("\nCard deactivated\n");
 
-exit:
+emv_exit:
+	emv_txn_destroy(&emv_txn);
+pcsc_exit:
 	pcsc_release(&pcsc);
 }
