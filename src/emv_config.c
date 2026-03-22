7b984e5c74c03614b9662727b16a0d701a68df9d
Leon Lynch <lynch.leon@gmail.com>
2026-03-22 14:48:34 +0100

    Implement initial EMV application configuration
    The EMV application configurations represent the supported applications
    during application discovery as well as encapsulate the application
    dependent data for each supported application. This implementation also
    allows supported applications to be enabled/disabled if necessary.
    
    The application discovery, application selection, and EMV configuration
    data retrieval have not yet been updated.

--- src/emv_config.c
+++ src/emv_config.c
@@ -22,8 +22,60 @@
 #include "emv_config.h"
 #include "emv.h"
 #include "emv_tlv.h"
+#include "emv_fields.h"
 
 #include <stddef.h>
+#include <stdlib.h> // For malloc() and free()
+#include <stdint.h>
+#include <string.h>
+
+static void emv_config_app_list_clear(struct emv_config_app_t** list)
+{
+	if (!list || !*list) {
+		return;
+	}
+
+	while (*list != NULL) {
+		struct emv_config_app_t* app = *list;
+		*list = app->next;
+
+		emv_tlv_list_clear(&app->data);
+		free(app);
+	}
+}
+
+static int emv_config_app_list_push(
+	struct emv_config_app_t** list,
+	struct emv_config_app_t* app
+)
+{
+	struct emv_config_app_t* itr;
+
+	if (!list) {
+		return -1;
+	}
+	if (!app) {
+		return -2;
+	}
+	if (app->next) {
+		return -3;
+	}
+
+	if (!*list) {
+		// Only app in list
+		*list = app;
+		return 0;
+	}
+
+	itr = *list;
+	while (itr->next) {
+		itr = itr->next;
+	}
+
+	// Last app in list
+	itr->next = app;
+	return 0;
+}
 
 int emv_config_clear(struct emv_config_t* config)
 {
@@ -32,6 +84,7 @@ int emv_config_clear(struct emv_config_t* config)
 	}
 
 	emv_tlv_list_clear(&config->data);
+	emv_config_app_list_clear(&config->supported_apps);
 
 	return 0;
 }
@@ -58,6 +111,74 @@ int emv_config_data_set(
 	r = emv_tlv_list_append(&ctx->config.data, data);
 	if (r) {
 		return EMV_ERROR_INTERNAL;
+	}
+
+	return 0;
+}
+
+int emv_config_app_create(
+	struct emv_ctx_t* ctx,
+	const void* aid,
+	unsigned int aid_len,
+	uint8_t asi,
+	struct emv_tlv_list_t* data,
+	struct emv_config_app_t** app
+)
+{
+	int r;
+	struct emv_config_app_t* tmp;
+
+	if (!ctx) {
+		return EMV_ERROR_INVALID_PARAMETER;
+	}
+	if (!aid || aid_len < 5 || aid_len > 16) {
+		return EMV_ERROR_INVALID_PARAMETER;
+	}
+	if (data && emv_tlv_list_has_duplicate(data)) {
+		return EMV_ERROR_INVALID_CONFIG;
+	}
+	if (app) {
+		*app = NULL;
+	}
+
+	tmp = malloc(sizeof(*tmp));
+	if (!tmp) {
+		return EMV_ERROR_INTERNAL;
+	}
+	memset(tmp, 0, sizeof(*tmp));
+
+	memcpy(tmp->aid, aid, aid_len);
+	tmp->aid_len = aid_len;
+	tmp->asi = asi;
+
+	r = emv_config_app_list_push(&ctx->config.supported_apps, tmp);
+	if (r) {
+		free(tmp);
+		return EMV_ERROR_INTERNAL;
+	}
+	if (data) {
+		r = emv_tlv_list_append(&tmp->data, data);
+		if (r) {
+			return EMV_ERROR_INTERNAL;
+		}
+	}
+	if (app) {
+		*app = tmp;
+	}
+
+	return 0;
+}
+
+int emv_config_app_set_enable(struct emv_config_app_t* app, bool enabled)
+{
+	if (!app) {
+		return EMV_ERROR_INVALID_PARAMETER;
+	}
+
+	if (enabled) {
+		app->asi &= ~EMV_ASI_DISABLED;
+	} else {
+		app->asi |= EMV_ASI_DISABLED;
 	}
 
 	return 0;
