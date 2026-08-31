14debbfd654a90f0233364ff9f162335d838ce22
Leon Lynch <lynch.leon@gmail.com>
2026-08-31 19:20:46 +0200

    ++Implement contactless PPSE and application selection

--- src/emv.c
+++ src/emv.c
@@ -720,7 +720,10 @@ int emv_build_combination_list(
 	}
 	if (r > 0) {
 		emv_debug_trace_msg("emv_tal_read_ppse() failed; r=%d", r);
-		emv_debug_info("Failed to process PPSE; terminate session");
+
+		// If PPSE fails, end application
+		// See EMV Contactless Book B v2.11, 3.3.2.3
+		emv_debug_info("Failed to process PPSE; try another card");
 		return EMV_OUTCOME_END_APPLICATION_TRY_ANOTHER_CARD;
 	}
 
@@ -877,10 +880,10 @@ int emv_build_combination_list(
 
 	}
 
-	// If there are no mutually supported applications, terminate session
+	// If there are no mutually supported applications, end application
 	// See EMV Contactless Book B v2.11, 3.3.2.7
 	if (emv_app_list_is_empty(app_list)) {
-		emv_debug_info("Combination list empty; terminate session");
+		emv_debug_info("Combination list empty; try another card");
 		return EMV_OUTCOME_END_APPLICATION_TRY_ANOTHER_CARD;
 	}
 
